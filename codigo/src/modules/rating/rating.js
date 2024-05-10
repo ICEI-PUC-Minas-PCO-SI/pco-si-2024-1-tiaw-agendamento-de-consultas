dc3239565b6a858dcc716b981685203b971ae16b
Kayque dos Santos Almeida <kayquesantos74@gmail.com>
2024-05-10 16:04:46 -0300

    Começando a função para pegar o valor das estrelas

--- codigo/src/modules/rating/rating.js
+++ codigo/src/modules/rating/rating.js
@@ -1,19 +1,28 @@
+import {Rating} from '/codigo/src/core/rating.js'
+
 const btn = document.querySelector("button", "#finalizar");
 const avaliation = document.querySelector('#avaliacao');
 const stars = document.querySelector("stars-rating");
 const question = document.querySelector('#pergunta');
 const voting = document.querySelector('#votacao');
-const thanks = document.querySelector('#thanks'); 
+const thanks = document.querySelector('#thanks');
+
+function starValue() {
+}
 
 btn.onclick =  () => {
+    
     question.style.display = 'none';
     voting.style.display = 'none';
     btn.style.display = 'none';
     avaliation.style.display = 'none'
     thanks.style.display = 'block';
 
-    setTimeout (() => {
+
+    /*setTimeout (() => {
         location.reload(); // Aqui o programa retornaria à página inicial após alguns segundos
-    }, 2000)
+    }, 2000)*/
 };
+
+// apos clicar no botão levar tudo ao firebase.
 
